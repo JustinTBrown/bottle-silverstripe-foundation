c15f415b9a9592dfb869adeded19d48c8716b9e8
Benny Lin <helloimbenny@gmail.com>
2015-08-23 15:13:14 -0600

    Removed commented lines and resolved #3

--- themes/project-theme/config.rb
+++ themes/project-theme/config.rb
@@ -1,12 +1,8 @@
-# require 'compass/import-once/activate'
-
-# Require any additional compass plugins here.
-# add_import_path "bower_components/foundation/scss"
-# require 'compass-notify'
 require "sass-globbing"
+require 'autoprefixer-rails'
 
 # Set this to the root of your project when deployed:
-http_path = "/"
+http_path = "/themes/project-theme"
 css_dir = 'css'
 sass_dir = 'scss'
 js_dir = 'javascript'
@@ -14,23 +10,8 @@ img_dir = 'images'
 
 add_import_path "bower_components/foundation/scss"
 
-# You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
-
-# To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
-
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = true
-
-
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
-# and then run:
-# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
-
-require 'autoprefixer-rails'
 
 on_stylesheet_saved do |file|
   css = File.read(file)
